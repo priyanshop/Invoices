1ad3af656ea05b1a711258288bfdb32ed22f82cc
priyanshop <priyansh4265@gmail.com>
2023-07-04 20:08:27 +0530

    carousel

--- src/Screens/Auth/SignUpScreen.tsx
+++ src/Screens/Auth/SignUpScreen.tsx
@@ -1,4 +1,4 @@
-import React, {useRef} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import {
   SafeAreaView,
   StatusBar,
@@ -8,14 +8,15 @@ import {
   TouchableOpacity,
   View,
 } from 'react-native';
-import Carousel from 'react-native-snap-carousel';
+import Carousel, {Pagination} from 'react-native-snap-carousel';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 import {getScreenDimensions} from '../../Helper/ScreenDimension';
 const screenDimensions = getScreenDimensions();
 const screenWidth = screenDimensions.width;
-const screenHeight = screenDimensions.height;
 
 function SignUpScreen(): JSX.Element {
   const carouselRef = useRef(null);
+  const [activeSlide, setActiveSlide] = useState(0);
 
   const pages = [{index: 1}, {index: 2}, {index: 3}];
   const handlePrevious = () => {
@@ -27,7 +28,6 @@ function SignUpScreen(): JSX.Element {
   };
 
   const renderItem = ({item}) => {
-    console.log(item.index);
     if (item.index === 1) {
       return (
         <View>
@@ -61,7 +61,7 @@ function SignUpScreen(): JSX.Element {
       );
     } else if (item.index === 2) {
       return (
-        <View style={{backgroundColor: 'red'}}>
+        <View>
           <Text style={styles.title}>Business Logo</Text>
           <Text style={styles.paragraph}>
             Appears on all invoices. Can be edited ny time
@@ -89,8 +89,27 @@ function SignUpScreen(): JSX.Element {
   return (
     <SafeAreaView style={styles.container}>
       <StatusBar backgroundColor={'#FF5733'} />
-      <View style={{flexDirection:'row',justifyContent:'space-between',marginHorizontal:10}}>
-
+      <View style={styles.header}>
+        <View style={{width: '20%'}}>
+          <TouchableOpacity onPress={handlePrevious}>
+            <Ionicons name="arrow-back" color={'#fff'} size={20} />
+          </TouchableOpacity>
+        </View>
+        <View style={{width: '60%'}}>
+          <Pagination
+            dotsLength={3}
+            activeDotIndex={activeSlide}
+            containerStyle={styles.paginationContainer}
+            dotStyle={styles.paginationDot}
+            inactiveDotOpacity={0.4}
+            inactiveDotScale={0.6}
+          />
+        </View>
+        <View style={{width: '20%'}}>
+          <Text onPress={handleNext} style={styles.finishTxt}>
+            Finish
+          </Text>
+        </View>
       </View>
       <Carousel
         ref={carouselRef}
@@ -98,9 +117,9 @@ function SignUpScreen(): JSX.Element {
         renderItem={renderItem}
         sliderWidth={screenWidth}
         itemWidth={screenWidth}
-        // inactiveSlideOpacity={1}
-        // inactiveSlideScale={1}
-        loop
+        onSnapToItem={(index: number) => setActiveSlide(index)}
+        inactiveSlideOpacity={1}
+        inactiveSlideScale={1}
       />
     </SafeAreaView>
   );
@@ -110,7 +129,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#FF5733',
-    // padding: 8,
   },
   paragraph: {
     marginVertical: 10,
@@ -147,13 +165,13 @@ const styles = StyleSheet.create({
     padding: 4,
   },
   emailInput: {
-    borderTopStartRadius: 5,
-    borderTopEndRadius: 5,
+    borderTopRightRadius: 5,
+    borderTopLeftRadius: 5,
     padding: 4,
   },
   phoneInput: {
-    borderBottomStartRadius: 5,
-    borderBottomEndRadius: 5,
+    borderBottomRightRadius: 5,
+    borderBottomLeftRadius: 5,
     borderTopWidth: 0.3,
     borderTopColor: 'grey',
     marginBottom: 10,
@@ -161,8 +179,8 @@ const styles = StyleSheet.create({
   },
   addressInput1: {
     padding: 4,
-    borderTopStartRadius: 5,
-    borderTopEndRadius: 5,
+    borderTopRightRadius: 5,
+    borderTopLeftRadius: 5,
   },
   addressInput: {
     borderTopWidth: 0.3,
@@ -170,14 +188,37 @@ const styles = StyleSheet.create({
     padding: 4,
   },
   lastAddressInput: {
-    borderBottomStartRadius: 5,
-    borderBottomEndRadius: 5,
+    borderBottomRightRadius: 5,
+    borderBottomLeftRadius: 5,
     borderTopWidth: 0.3,
     borderTopColor: 'grey',
     marginBottom: 10,
     padding: 4,
   },
   businessName: {marginBottom: 10, borderRadius: 5},
+  paginationContainer: {
+    paddingVertical: 8,
+  },
+  paginationDot: {
+    width: 15,
+    height: 15,
+    borderRadius: 7.5,
+    marginHorizontal: 2,
+    backgroundColor: 'rgba(0, 0, 0, 0.92)',
+  },
+  finishTxt: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    color: '#fff',
+  },
+  header: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginHorizontal: 10,
+    paddingVertical: 10,
+    marginVertical: 10,
+  },
 });
 
 export default SignUpScreen;
