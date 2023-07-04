88a359b8fd27c72def28aad1561cf78cfd0f8d0d
priyanshop <priyansh4265@gmail.com>
2023-07-04 20:21:21 +0530

    lib

--- index.js
+++ index.js
@@ -2,8 +2,12 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
+import { AppRegistry } from 'react-native';
 import App from './App';
-import {name as appName} from './app.json';
+import { name as appName } from './app.json';
+
+if (__DEV__) {
+    import("./config/ReactotronConfig").then(() => { });
+}
 
 AppRegistry.registerComponent(appName, () => App);
