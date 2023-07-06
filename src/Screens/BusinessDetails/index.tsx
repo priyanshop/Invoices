b4b85b2f81ad5bac268eb066dc70f79aab953f8b
priyanshop <priyansh4265@gmail.com>
2023-07-06 17:19:20 +0530

    image picker

--- src/Screens/BusinessDetails/index.tsx
+++ src/Screens/BusinessDetails/index.tsx
@@ -1,37 +1,58 @@
 import React, {useState} from 'react';
-import {View, SectionList, Text, StyleSheet, TextInput} from 'react-native';
-import DatePicker from 'react-native-date-picker';
+import {
+  View,
+  SectionList,
+  Text,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+  Image,
+} from 'react-native';
 import Feather from 'react-native-vector-icons/Feather';
+import ImagePickerComponent from '../../CustomComponent/ImagePickerComponent';
 
 const BusinessDetails = () => {
   const [openModal, setOpenModal] = useState(false);
-  const [selectedTerm, setSelectedTerm] = useState('Due on receipt');
-  const [invoiceNumber, setInvoiceNumber] = useState('');
-  const [date, setDate] = useState(new Date());
-  const [poNumber, setPoNumber] = useState('');
-  const [dueDate, setDueDate] = useState(null);
-  const [openDate, setOpenDate] = useState(false);
+  const [BusinessImage, setBusinessImage] = useState(null);
+  const [businessName, setBusinessName] = useState('');
+  const [ownerName, setOwnerName] = useState('');
+  const [businessNumber, setBusinessNumber] = useState('');
+  const [address1, setAddress1] = useState('');
+  const [address2, setAddress2] = useState('');
+  const [address3, setAddress3] = useState('');
+  const [email, setEmail] = useState('');
+  const [Phone, setPhone] = useState('');
+  const [Mobile, setMobile] = useState('');
+  const [Website, setWebsite] = useState('');
 
+  const closeBottomSheet = () => {
+    setOpenModal(!openModal);
+  };
   return (
     <View style={styles.mainContainer}>
-      <View style={{borderRadius: 8, backgroundColor: '#fff'}}>
-        <View
-          style={{
-            flexDirection: 'row',
-            padding: 8,
-            backgroundColor: 'grey',
-            borderTopLeftRadius: 8,
-            borderTopRightRadius: 8,
-          }}>
-          <Text style={{fontSize: 17, color: '#fff', fontWeight: '500'}}>Business Logo</Text>
+      <View style={styles.businessContainer}>
+        <View style={styles.header}>
+          <Text style={styles.headerText}>Business Logo</Text>
         </View>
-        <View style={{flexDirection:'row',justifyContent:'center',alignItems:'center',padding:10}}>
-        <Feather name="camera" size={70} color="#d4d4d4" />
+        <View style={styles.content}>
+          <TouchableOpacity onPress={closeBottomSheet}>
+            {BusinessImage ? (
+              <Image
+                source={{uri: BusinessImage}}
+                resizeMode="contain"
+                style={styles.businessImage}
+              />
+            ) : (
+              <Feather name="camera" style={styles.cameraIcon} />
+            )}
+          </TouchableOpacity>
         </View>
       </View>
       <View style={styles.mainContain}>
         <View style={styles.rowView}>
           <TextInput
+            value={businessName}
+            onChangeText={setBusinessName}
             style={{...styles.titleTxt, flex: 1, textAlign: 'left'}}
             placeholder="Business Name"
             placeholderTextColor={'grey'}
@@ -39,6 +60,8 @@ const BusinessDetails = () => {
         </View>
         <View style={styles.rowView}>
           <TextInput
+            value={ownerName}
+            onChangeText={setOwnerName}
             style={{...styles.titleTxt, flex: 1, textAlign: 'left'}}
             placeholder="Business Owner Name"
             placeholderTextColor={'grey'}
@@ -46,6 +69,8 @@ const BusinessDetails = () => {
         </View>
         <View style={styles.rowView}>
           <TextInput
+            value={businessNumber}
+            onChangeText={setBusinessNumber}
             style={{...styles.titleTxt, flex: 1, textAlign: 'left'}}
             placeholder="Business Number"
             placeholderTextColor={'grey'}
@@ -55,6 +80,8 @@ const BusinessDetails = () => {
       <View style={styles.mainContain}>
         <View style={styles.rowView}>
           <TextInput
+            value={address1}
+            onChangeText={setAddress1}
             style={{...styles.titleTxt, flex: 1, textAlign: 'left'}}
             placeholder="Address Line 1"
             placeholderTextColor={'grey'}
@@ -62,6 +89,8 @@ const BusinessDetails = () => {
         </View>
         <View style={styles.rowView}>
           <TextInput
+            value={address2}
+            onChangeText={setAddress2}
             style={{...styles.titleTxt, flex: 1, textAlign: 'left'}}
             placeholder="Address Line 2"
             placeholderTextColor={'grey'}
@@ -69,6 +98,8 @@ const BusinessDetails = () => {
         </View>
         <View style={styles.rowView}>
           <TextInput
+            value={address3}
+            onChangeText={setAddress3}
             style={{...styles.titleTxt, flex: 1, textAlign: 'left'}}
             placeholder="Address Line 3"
             placeholderTextColor={'grey'}
@@ -78,6 +109,8 @@ const BusinessDetails = () => {
       <View style={styles.mainContain}>
         <View style={styles.rowView}>
           <TextInput
+            value={email}
+            onChangeText={setEmail}
             style={{...styles.titleTxt, flex: 1, textAlign: 'left'}}
             placeholder="Email"
             placeholderTextColor={'grey'}
@@ -85,6 +118,8 @@ const BusinessDetails = () => {
         </View>
         <View style={styles.rowView}>
           <TextInput
+            value={Phone}
+            onChangeText={setPhone}
             style={{...styles.titleTxt, flex: 1, textAlign: 'left'}}
             placeholder="Phone"
             placeholderTextColor={'grey'}
@@ -92,6 +127,8 @@ const BusinessDetails = () => {
         </View>
         <View style={styles.rowView}>
           <TextInput
+            value={Mobile}
+            onChangeText={setMobile}
             style={{...styles.titleTxt, flex: 1, textAlign: 'left'}}
             placeholder="Mobile"
             placeholderTextColor={'grey'}
@@ -99,12 +136,19 @@ const BusinessDetails = () => {
         </View>
         <View style={styles.rowView}>
           <TextInput
+            value={Website}
+            onChangeText={setWebsite}
             style={{...styles.titleTxt, flex: 1, textAlign: 'left'}}
             placeholder="Website"
             placeholderTextColor={'grey'}
           />
         </View>
       </View>
+      <ImagePickerComponent
+        openModal={openModal}
+        closeBottomSheet={closeBottomSheet}
+        setImage={setBusinessImage}
+      />
     </View>
   );
 };
@@ -130,6 +174,48 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     padding: 8,
     marginVertical: 5,
+  },
+  bottomSheetContainer: {
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    right: 0,
+    height: '18%',
+    backgroundColor: '#F8FAFE',
+    width: '100%',
+    paddingBottom: 0,
+    paddingHorizontal: 0,
+  },
+  innerView: {flex: 1, paddingHorizontal: 8},
+  businessContainer: {
+    borderRadius: 8,
+    backgroundColor: '#fff',
+  },
+  header: {
+    flexDirection: 'row',
+    padding: 8,
+    backgroundColor: 'grey',
+    borderTopLeftRadius: 8,
+    borderTopRightRadius: 8,
+  },
+  headerText: {
+    fontSize: 17,
+    color: '#fff',
+    fontWeight: '500',
+  },
+  content: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 10,
+  },
+  businessImage: {
+    width: 200,
+    height: 250,
+  },
+  cameraIcon: {
+    fontSize: 50,
+    color: '#d4d4d4',
   },
 });
 
